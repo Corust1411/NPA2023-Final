928f31d0d4e210b0da805a4bd6b493e49f31ae6a
Corust1411 <sirapob.dar@gmail.com>
2024-10-24 23:24:13 +0700

    update : update creaste restconf function

--- restconf_final.py
+++ restconf_final.py
@@ -3,28 +3,49 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 # Router IP Address is 10.0.15.189
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+api_url = "https://10.0.15.189/restconf/data/ietf-interfaces:interfaces/interface=Loopback65070236"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
+headers = {
+    "Accept": "application/yang-data+json", 
+    "Content-type":"application/yang-data+json"
+    }
 basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback65070236",
+            "description": "My second RESTCONF loopback",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "172.30.236.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            },
+            "ietf-ip:ipv6": {}
+        }
+    }
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        api_url, 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
-
-    if(resp.status_code >= 200 and resp.status_code <= 299):
+    if(resp.status_code == 204):
+        print("STATUS : {}".format(resp.status_code))
+        return "Cannot create: Interface loopback 65070236"
+    elif(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface loopback 65070236 is created successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
 
